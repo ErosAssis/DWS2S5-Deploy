a3290fb2d5320f71bd597537c5ca835299caaae0
ErosAssis <erosassis5@gmail.com>
2024-07-04 01:48:12 -0300

    f-commit

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -4,28 +4,69 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
     public function index(){
 
-        $nome = "Eros";
-        $idade = 20;
-    
-        $arr = [1, 2, 3, 4, 5];
-    
-        $nomes = ["Victor", "Eros", "Arthur", "João Otávio"];
-    
-        return view('welcome', 
-            [
-                'nome' =>  $nome,
-                'idade' => $idade, 
-                'profissao' => "Garoto de Programa",
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]);
+        $search = request ('search');
+
+        if($search){
+            
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+        $events = Event::all();
+        }
+        return view('welcome', ['events' => $events, 'search' => $search]);
     }
 
     public function create() {
         return view('events.create');
     }
+
+    public function store(Request $request){
+
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->date = $request->date;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+        $event->items = $request->items;
+
+        $event->save();
+
+        //Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()) {
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
+        $event->save();
+
+        return redirect('/')->with('msg', 'Evento Criado com Sucesso!');
+    }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+        
+    }
+
 }
+
+
